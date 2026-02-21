ef151af1f6b8ad6f4c6350f3e5184de5e6f29db8
Exurum <258735700+Exurum@users.noreply.github.com>
2026-02-20 21:44:19 -0800

    Correct constants and comments for Chocobo/Chocobot.

--- FFA_ASM/src/data/npc/main.asm
+++ FFA_ASM/src/data/npc/main.asm
@@ -26,8 +26,8 @@ npcDataTable:
     NPC_DATA $d9, $05, $20, $08, gfxNpcAmanda, tileorderNpc, data_03_7f62, $00, $01, $01, $01, $01, $00, $06, $06, $06, $06, script_0206, $0000 ;; 03:5fd2 NPC_AMANDA_FOLLOWING
     NPC_DATA $d9, $06, $20, $08, gfxNpcLester, tileorderNpc, data_03_7f62, $00, $01, $01, $01, $01, $00, $06, $06, $06, $06, script_01ff, $0000 ;; 03:5fea NPC_LESTER_FOLLOWING
     NPC_DATA $d9, $07, $20, $08, gfxNpcMarcie, tileorderNpc, data_03_7f62, $00, $01, $01, $01, $01, $00, $06, $06, $06, $06, script_0200, $0000 ;; 03:6002 NPC_MARCIE_FOLLOWING
-    NPC_DATA $d9, $08, $20, $08, gfxNpcChocobot, tileorderNpc, data_03_7f62, $00, $01, $01, $01, $00, $00, $05, $05, $05, $05, script_0470, $0000 ;; 03:601a NPC_CHOCOBOT_FOLLOWING
     NPC_DATA $d9, $09, $20, $08, gfxNpcChocobo, tileorderNpc, data_03_7f62, $00, $01, $01, $01, $00, $00, $05, $05, $05, $05, script_0470, $0000 ;; 03:6032 NPC_CHOCOBO_FOLLOWING
+    NPC_DATA $d9, $08, $20, $08, gfxNpcChocobot, tileorderNpc, data_03_7f62, $00, $01, $01, $01, $00, $00, $05, $05, $05, $05, script_0470, $0000 ;; 03:601a NPC_CHOCOBOT_FOLLOWING
     NPC_DATA $91, $25, $50, $08, gfxEnemyWerewolf, tileorderNpc, data_03_7cfe, $00, $01, $00, $00, $01, $00, $0f, $05, $05, $05, script_003d, $0000 ;; 03:604a NPC_WEREWOLF_1
     NPC_DATA $81, $5e, $6e, $01, gfxBlankTiles09, tileorderNpc, data_03_7f3e, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, script_01f1, $0000 ;; 03:6062 NPC_INV_CURE
     NPC_DATA $87, $0c, $78, $02, gfxChest, tileorderNpc, chestMetaspriteTable, $00, $00, $00, $00, $00, $00, $00, $00, $00, $00, script_000b, $0000 ;; 03:607a NPC_CHEST_1
